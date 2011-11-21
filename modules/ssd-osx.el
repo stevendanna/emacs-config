aa476626c2343d606468c53bcae1f8bd63bb8810
Steven Danna <steve@opscode.com>
2011-11-21 08:00:16 -0800

    Fix type

--- modules/ssd-osx.el
+++ modules/ssd-osx.el
@@ -1,3 +1,3 @@
 ;; Random bits that make everything work on OSX
 (if (eq system-type 'darwin)
-    (setq exec-path (cons "/usr/local/bin" exex-path)))
+    (setq exec-path (cons "/usr/local/bin" exec-path)))
