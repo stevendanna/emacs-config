d6f2bff8155622730789606058b64a4bf1173588
Steven Danna <StevenSDanna@gmail.com>
2011-11-22 00:03:44 -0800

    Fixup confluence config a bit

--- modules/ssd-confluence.el
+++ modules/ssd-confluence.el
@@ -12,12 +12,12 @@
 ;; Functions to move between corp and community confluence setups
 (defun go-corporate()
   (interactive)
-  (custom-set-variables
-   '(confluence-url "https://wiki.corp.opscode.com/rpc/xmlrpc")
-   '(confluence-default-space-alist (list (cons confluence-url "CORP")))))
+  (setq
+   confluence-url "https://wiki.corp.opscode.com/rpc/xmlrpc"
+   confluence-default-space-alist (list (cons confluence-url "CORP"))))
 
 (defun go-community()
   (interactive)
-  (custom-set-variables
-   '(confluence-url "https://wiki.opscode.com/rpc/xmlrpc")
-   '(confluence-default-space-alist (list (cons confluence-url "chef")))))
+  (setq
+   confluence-url "https://wiki.opscode.com/rpc/xmlrpc"
+   confluence-default-space-alist (list (cons confluence-url "chef"))))
