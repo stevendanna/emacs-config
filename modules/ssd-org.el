011c2fa3159b5aeb051d9d2e0bd0ced10f47559c
Steven Danna <StevenSDanna@gmail.com>
2011-11-26 14:42:22 -0800

    org+deft setup

--- modules/ssd-org.el
+++ modules/ssd-org.el
@@ -1,4 +1,13 @@
 ;; org-mode settings
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
+(setq org-log-done t
+      org-agenda-files '("~/org/"
+                         "~/org/deft"))
+
+p;; deft for quick note taking
+(require 'deft)
+(setq deft-extension "org"
+      deft-directory "~/org/deft/"
+      deft-text-mode 'org-mode)
+(global-set-key (kbd "\C-cd") 'deft)
