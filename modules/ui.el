3cdb0efcee8956b51ac2cfe3ae628efa498aab4f
Steven Danna <steve@opscode.com>
2011-11-26 18:08:42 -0800

    Change font size based on platform

--- modules/ui.el
+++ modules/ui.el
@@ -6,7 +6,9 @@
 (set-face-attribute 'mode-line nil :box nil)
 (set-face-attribute 'mode-line-inactive nil :box nil)
 ;; font
-(set-face-attribute 'default nil :height 100 :family "Monaco")
+(if (eq system-type 'darwin)
+    (set-face-attribute 'default nil :height 120 :family "Monaco")
+    (set-face-attribute 'default nil :height 90 :family "Monaco"))
 
 ;; Remove uncessary UI elements
 (scroll-bar-mode -1)
