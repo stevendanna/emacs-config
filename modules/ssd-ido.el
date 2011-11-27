6a398446998e7c5fa5f260deba584071cd10ad19
Steven Danna <StevenSDanna@gmail.com>
2011-11-26 17:56:35 -0800

    Minor update to ido config

--- modules/ssd-ido.el
+++ modules/ssd-ido.el
@@ -7,7 +7,6 @@
       confirm-nonexistent-file-or-buffer nil
       ido-auto-merge-work-directories-length nil
       ido-create-new-buffer 'always
-      ido-use-filename-at-point 'guess
       ido-use-virtual-buffers t
       ido-handle-duplicate-virtual-buffers 2
       ido-max-prospects 10)
