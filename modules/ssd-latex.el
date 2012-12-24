5760ccda0631edb53354e7a75ec97fd799754a4b
Steven Danna <steve@opscode.com>
2012-12-24 08:38:27 -0800

    [modules/ssd-latex] Add better OS X support.

--- modules/ssd-latex.el
+++ modules/ssd-latex.el
@@ -3,3 +3,13 @@
 (setq TeX-parse-self t)
 (add-hook 'LaTeX-mode-hook 'TeX-PDF-mode)
 (add-hook 'LaTeX-mode-hook 'turn-on-auto-fill)
+
+
+;; OS X Madness
+(if (eq system-type 'darwin)
+    (progn
+      (setenv "PATH"
+              (concat "/usr/texbin" ":"
+                      (getenv "PATH")))
+      (setq TeX-view-program-list '(("Shell Default" "open %o")))
+      (setq TeX-view-program-selection '((output-pdf "Shell Default")))))
