e176095b60f14ebbf8f426719c486cd36807ae60
Steven Danna <steve@chef.io>
2016-10-30 13:13:06 +0000

    Update Erlang config
    Eventually I should figure out how to get the paths to the currently
    configured erln8 build, but this works for now.
    
    Signed-off-by: Steven Danna <steve@chef.io>

--- modules/ssd-erlang.el
+++ modules/ssd-erlang.el
@@ -1,25 +1,30 @@
-(add-to-list 'load-path "~/.erln8.d/otps/OTP-17.5/dist/lib/erlang/lib/tools-2.7.2/emacs/")
-(add-to-list 'exec-path "~/.erln8.d/otps/OTP-17.5/dist/bin/")
-(setq erlang-root-dir "~/.erln8.d/otps/OTP-17.5/dist/bin/")
+(add-to-list 'auto-mode-alist '("rebar.config$" . erlang-mode))
+
+(add-to-list 'load-path "~/.erln8.d/otps/OTP-18.2.3/dist/lib/erlang/lib/tools-2.7.2/emacs/")
+(add-to-list 'exec-path "~/.erln8.d/otps/OTP-18.2.3/dist/bin/")
+(setq erlang-root-dir "~/.erln8.d/otps/OTP-18.2.3/dist/")
+(setq erlang-flymake-get-include-dirs-function 'my-erlang-flymake-get-include-dirs)
+(setq erlang-flymake-get-code-path-dirs-function 'my-erlang-flymake-get-code-dirs)
+(setq erlang-man-dirs (list '("Man - Commands" "lib/erlang/man/man1" t)
+                            '("Man - Modules" "lib/erlang/man/man3" t)
+                            '("Man - Files" "lib/erlang/man/man4" t)
+                            '("Man - Applications" "lib/erlang/man/man6" t)))
+
 
 (require 'erlang-start)
-(require 'edts-start)
+(require 'erlang-flymake)
+(add-hook 'erlang-mode-hook 'linum-mode)
+(add-hook 'erlang-mode-hook 'flymake-mode)
 
-(require 'projmake-mode)
-(add-hook 'erlang-mode-hook 'my-erlang-mode-hook)
 
-;; Bonus, projmake-mode for rebar3
-(setq projmake-project-descs
-      '(("Rebar" "rebar.config" "nice -n5 rebar3 compile")))
+(defun my-erlang-flymake-get-code-dirs ()
+  (append (list (concat (erlang-flymake-get-app-dir) "ebin"))
+          (file-expand-wildcards (concat (erlang-flymake-get-app-dir) "_build/default/lib/*/ebin"))
+          (file-expand-wildcards (concat (erlang-flymake-get-app-dir) "_build/test/lib/*/ebin"))))
 
-;; stole this fun from seth falcon, it ain't broke so...
-(defun my-erlang-mode-hook ()
-  (if (buffer-file-name)
-      ;; projmake breaks org-mode code block editing. need to
-      ;; investigate
-      (progn
-        (projmake-mode)
-        (projmake-search-load-project)
-        (projmake-enable))
-    (message "skipping projmake for ephemeral buffer"))
-  )
+(defun my-erlang-flymake-get-include-dirs ()
+  (list (concat (erlang-flymake-get-app-dir) "apps")
+        (concat (erlang-flymake-get-app-dir) "include")
+        (concat (erlang-flymake-get-app-dir) "_build/default/lib")
+        (concat (erlang-flymake-get-app-dir) "_build/test/lib")
+        (concat (erlang-flymake-get-app-dir) "deps")))
